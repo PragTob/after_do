78c3db180add6a89b10f626a4441b3ad40fbb359
Tobias Pfeiffer <tobias.pfeiffer@student.hpi.uni-potsdam.de>
2013-11-10 13:48:47 +0100

    Ignore spec folder for coverage data

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,5 +4,9 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   Coveralls::SimpleCov::Formatter,
   SimpleCov::Formatter::HTMLFormatter
 ]
-SimpleCov.start
+
+SimpleCov.start do
+  add_filter '/spec/'
+end
+
 require 'after_do'
