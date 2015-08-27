e685bd2e0ec4455727d356365dd264b06b8c3197
David Begin <davidmichaelbe@gmail.com>
2015-08-26 22:20:46 -0700

    added some more comments to getting_a_hold example

--- samples/getting_a_hold.rb
+++ samples/getting_a_hold.rb
@@ -17,10 +17,24 @@ end
 Example.extend AfterDo
 
 Example.after :zero do puts 'Hello!' end
+
+# If the callback method takes no arguments
+# then the first argument passed to the block
+# with will the instance of the class you are in.
 Example.after :zero do |obj| puts obj.value end
+
+# Callback methods can take arguments.
 Example.after :two do |first, second| puts first + ' ' + second end
+
+# If the callback takes arguments, the last argument, after the required
+# arguments will be the instance of the class.
 Example.after :two do |a, b, obj| puts a + ' ' + b + ' ' + obj.value end
-Example.after :two do |*, obj| puts 'just ' +  obj.value end
+
+# You can also you use the * to suck up all the required arguments
+Example.after :two do |*args, obj|
+  puts "args passed to callback: #{args.join(', ')}"
+  puts 'just ' +  obj.value
+end
 
 e = Example.new
 e.zero
@@ -30,4 +44,5 @@ e.two 'one', 'two'
 # some value
 # one two
 # one two some value
+# args passed to callback: one, two
 # just some value
